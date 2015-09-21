d19e38fa5861a57188099819ac259df5535710ba
LONGMAN <akalongman@gmail.com>
2015-09-21 17:17:12 +0400

    Started translation to ka-GE (Georgian)

---
+++ ka/auth.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => 'მომხმარებლის სახელი და პაროლი არ ემთხვევა ერთმანეთს.',
+    'throttle' => 'თქვენ უკვე რამდენჯერმე შეიყვანეთ არასწორად მომხმარებლის სახელი და პაროლი. გთხოვთ ხელახლა სცადოთ :seconds წამში.',
+];
---
+++ ka/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "პაროლი არ უნდა იყოს 6 სიმბოლოზე ნაკლები და უნდა ემთხვეოდეს გამეორებით პაროლს.",
+    "reset"    => "თქვენი პაროლი დარესეტდა!",
+    "sent"     => "პაროლის დარესეტების ლინკი გამოგზავნილია!",
+    "token"    => "პაროლის დარესეტების კოდი არასწორადაა შეყვანილი.",
+    "user"     => "მითითებული ელ. ფოსტის მისამართით მომხმარებლის პოვნა ვერ მოხერხდა.",
+
+];
